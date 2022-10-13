8a16daba6b3bf4d0939544be6ad6275933cc1b69
Iryna <pshonka@gmail.com>
2022-10-13 19:41:12 +0200

    add comment

--- src/main/java/com/example/bookapi/BookController.java
+++ src/main/java/com/example/bookapi/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// Binde Book-Api https://my-json-server.typicode.com/Flooooooooooorian/BookApi/books
 @RestController
 @RequestMapping("/api")
 public class BookController {
